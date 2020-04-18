f28e6e72db81ab01ae4b90767deb87935ec7a595
gaowei <1qaz2wsx>
2020-04-18 10:13:24 +0800

    update: 参数位置调换

--- vars/pipeline_java.groovy
+++ vars/pipeline_java.groovy
@@ -57,120 +57,120 @@ def call(Map map) {
                 }
             }
 
-            stage('编译') {
-                steps {
-                    script {
-                        mvn { settings ->
-                            def cmd = isDev() ? 'deploy' : 'deploy'
-                            sh "mvn -s ${settings} clean ${cmd} -B -Dfile.encoding=UTF-8 -Dmaven.test.skip=true -U"
-                        }
-                    }
-                }
-            }
-
-            stage('Sonar分析') {
-                when {
-                    expression {
-                        return !isDev() && false
-                    }
-                }
-                steps {
-                    script {
-                        withSonarQubeEnv('sonar'){
-                            docker.image('mercuriete/sonar-scanner:3.2.0.1227').inside('-v /root/.sonar:/root/.sonar') {
-                                sh "sonar-scanner -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}  -Dsonar.projectKey=${env.APP} -Dsonar.projectName=${env.APP} -Dsonar.sources=. -Dsonar.java.binaries=."
-                            }
-                        }
-                    }
-                }
-            }
-
-            stage('单元测试') {
-                when {
-                    expression {
-                        return false
-                    }
-                }
-                steps {
-                    allure([
-                        disabled: false,
-                        includeProperties: false,
-                        jdk: '',
-                        reportBuildPolicy: 'ALWAYS',
-                        results: [[path: 'output/allure']]
-                    ])
-                }
-            }
-
-            stage('自动化测试') {
-                failFast true
-                parallel {
-                    stage('UI自动化测试') {
-                        steps {
-                            echo "并行1"
-                        }
-                    }
-                    stage('性能自动化测试') {
-                        steps {
-                            echo "并行二"
-                        }
-                    }
-                    stage('接口自动化测试') {
-                        steps {
-                            echo "In stage Nested 1 within Branch C"
-                        }
-                    }
-                }
-            }
-
-            stage('推送镜像') {
-                steps {
-                    script {
-                        def isBinaryConfig = isBinaryConfig(map)
-                        def dockerfile = isBinaryConfig ? 'dockerfile_config' : 'dockerfile'
-
-                        configFileProvider([configFile(fileId: dockerfile, variable: 'DOCKER_FILE')]) {
-                            docker.withRegistry("$HARBOR_URL", "harbor") {
-                                def args = "--no-cache --build-arg JAR_PATH=${ARTIFACT} --build-arg JAR_NAME=${APP} "
-                                if (isBinaryConfig) {
-                                    args = args + "--build-arg CONFIG_PATH=config/${env.APP}"
-                                }
-                                log.debug("args = ${args}")
-
-                                def app = docker.build("$IMAGE_NAME", "${args} -f ${DOCKER_FILE} .")
-                                app.push()
-                            }
-                        }
-                        sh "docker rmi -f $IMAGE_NAME"
-                    }
-                }
-            }
-
-            stage("K8S部署") {
-                steps{
-                    configFileProvider([configFile(fileId: "${params.BUILD_ENV}-config", variable: 'config')]) {
-                        sh "docker run --rm harbor.jq.cn/library/kubectl:1.15 -n ${env.NS} set image deployment ${env.APP} ${env.APP}=${IMAGE_NAME}"
-                    }
-                }
-            }
-
-            stage('同步阿里云') {
-                when {
-                    expression {
-                        return isUat()
-                    }
-                }
-                steps {
-                    script {
-                        def response = httpRequest(
-                            url: "${env.JENKINS_URL}/view/PRD/job/aliyun-harbor/buildWithParameters?token=${env.PORTAL_TOKEN}&app=${env.APP}&imageId=${BUILD_ID}",
-                            httpMode: 'GET'
-                        )
-                        println('Status: '+response.status)
-                        println('Response: '+response.content)
-                    }
-                }
-            }
+//            stage('编译') {
+//                steps {
+//                    script {
+//                        mvn { settings ->
+//                            def cmd = isDev() ? 'deploy' : 'deploy'
+//                            sh "mvn -s ${settings} clean ${cmd} -B -Dfile.encoding=UTF-8 -Dmaven.test.skip=true -U"
+//                        }
+//                    }
+//                }
+//            }
+//
+//            stage('Sonar分析') {
+//                when {
+//                    expression {
+//                        return !isDev() && false
+//                    }
+//                }
+//                steps {
+//                    script {
+//                        withSonarQubeEnv('sonar'){
+//                            docker.image('mercuriete/sonar-scanner:3.2.0.1227').inside('-v /root/.sonar:/root/.sonar') {
+//                                sh "sonar-scanner -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}  -Dsonar.projectKey=${env.APP} -Dsonar.projectName=${env.APP} -Dsonar.sources=. -Dsonar.java.binaries=."
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//            stage('单元测试') {
+//                when {
+//                    expression {
+//                        return false
+//                    }
+//                }
+//                steps {
+//                    allure([
+//                        disabled: false,
+//                        includeProperties: false,
+//                        jdk: '',
+//                        reportBuildPolicy: 'ALWAYS',
+//                        results: [[path: 'output/allure']]
+//                    ])
+//                }
+//            }
+//
+//            stage('自动化测试') {
+//                failFast true
+//                parallel {
+//                    stage('UI自动化测试') {
+//                        steps {
+//                            echo "并行1"
+//                        }
+//                    }
+//                    stage('性能自动化测试') {
+//                        steps {
+//                            echo "并行二"
+//                        }
+//                    }
+//                    stage('接口自动化测试') {
+//                        steps {
+//                            echo "In stage Nested 1 within Branch C"
+//                        }
+//                    }
+//                }
+//            }
+//
+//            stage('推送镜像') {
+//                steps {
+//                    script {
+//                        def isBinaryConfig = isBinaryConfig(map)
+//                        def dockerfile = isBinaryConfig ? 'dockerfile_config' : 'dockerfile'
+//
+//                        configFileProvider([configFile(fileId: dockerfile, variable: 'DOCKER_FILE')]) {
+//                            docker.withRegistry("$HARBOR_URL", "harbor") {
+//                                def args = "--no-cache --build-arg JAR_PATH=${ARTIFACT} --build-arg JAR_NAME=${APP} "
+//                                if (isBinaryConfig) {
+//                                    args = args + "--build-arg CONFIG_PATH=config/${env.APP}"
+//                                }
+//                                log.debug("args = ${args}")
+//
+//                                def app = docker.build("$IMAGE_NAME", "${args} -f ${DOCKER_FILE} .")
+//                                app.push()
+//                            }
+//                        }
+//                        sh "docker rmi -f $IMAGE_NAME"
+//                    }
+//                }
+//            }
+//
+//            stage("K8S部署") {
+//                steps{
+//                    configFileProvider([configFile(fileId: "${params.BUILD_ENV}-config", variable: 'config')]) {
+//                        sh "docker run --rm harbor.jq.cn/library/kubectl:1.15 -n ${env.NS} set image deployment ${env.APP} ${env.APP}=${IMAGE_NAME}"
+//                    }
+//                }
+//            }
+//
+//            stage('同步阿里云') {
+//                when {
+//                    expression {
+//                        return isUat()
+//                    }
+//                }
+//                steps {
+//                    script {
+//                        def response = httpRequest(
+//                            url: "${env.JENKINS_URL}/view/PRD/job/aliyun-harbor/buildWithParameters?token=${env.PORTAL_TOKEN}&app=${env.APP}&imageId=${BUILD_ID}",
+//                            httpMode: 'GET'
+//                        )
+//                        println('Status: '+response.status)
+//                        println('Response: '+response.content)
+//                    }
+//                }
+//            }
 
         }
 
