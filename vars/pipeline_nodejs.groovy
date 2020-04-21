51e60338120ccc845757f7d7f3f0c05df48f742f
gaowei <1qaz2wsx>
2020-04-21 18:11:35 +0800

    debug

--- vars/pipeline_nodejs.groovy
+++ vars/pipeline_nodejs.groovy
@@ -26,8 +26,8 @@ def call(Map map) {
         }
 
         parameters {
+            choice(name: 'BUILD_ENV', choices: 'dev\ntest\nuat', description: '请选择部署环境:')
             choice(name: 'BUILD_BRANCH', choices: 'dev\nrelease', description: '请选择分支:')
-            choice(name: 'BUILD_ENV', choices: 'mit\nsit\nuat', description: '请选择部署环境:')
         }
 
         stages {
