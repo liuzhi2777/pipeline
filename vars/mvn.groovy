0f7d627748196fe7c13cbec0e33744ea0d38092b
高伟 <gaowei@fengjinggroup.com>
2020-05-25 23:33:50 +0800

    更新 mvn.groovy

--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,7 +1,7 @@
 def call(mvnExec) {
     configFileProvider(
         [configFile(fileId: "${params.BUILD_ENV}-maven-global-settings", variable: 'MAVEN_SETTINGS_ENV')]) {
-        docker.image('maven:3-jdk-8-alpine').inside('-v /root/.m2:/root/.m2') {
+        docker.image('maven:3-jdk-8-alpine').inside('-v /root/.m2/${params.BUILD_ENV}:/root/.m2') {
             log.debug("MAVEN_SETTINGS: ${MAVEN_SETTINGS_ENV}")
             mvnExec("${MAVEN_SETTINGS_ENV}")
         }
